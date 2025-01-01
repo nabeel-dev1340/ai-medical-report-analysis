a59bce51d8771eca1ac2782d524d4699b6ee022e
Syed Nabeel Sharafat <nabeelsharafat@gmail.com>
2025-01-01 16:00:04 +0500

    placeholder logo

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="flex items-center space-x-2">
+      <div className="bg-white p-2 rounded-lg shadow-md">
+        <span className="text-blue-600 text-2xl font-bold">M</span>
+      </div>
+      <span className="text-3xl font-bold text-white">edAI</span>
+    </div>
+  );
+};
+
+export default Logo;
