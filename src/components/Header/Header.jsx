9e5b8f7041972b3e75e68c412675bb19d137c180
Syed Nabeel Sharafat <nabeelsharafat@gmail.com>
2025-01-01 16:09:04 +0500

    logo points to home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import Logo from "../Logo/Logo";
 import DesktopNav from "../Nav/DesktopNav";
 import MobileNav from "../Nav/MobileNav";
 import MenuToggle from "../Nav/MenuToggle";
+import NavLink from "../Nav/NavLink";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -14,7 +15,9 @@ const Header = () => {
       <div className="mx-auto max-w-7xl">
         <div className="relative z-10 px-6 py-4 lg:py-5">
           <div className="flex items-center justify-between">
-            <Logo />
+            <NavLink href="/">
+              <Logo />
+            </NavLink>
             <DesktopNav />
             <MenuToggle
               isOpen={isMenuOpen}
